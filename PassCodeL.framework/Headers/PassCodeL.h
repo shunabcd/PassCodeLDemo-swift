00b3b65e4d009b50f66cfb00a9d8855dc75f7506
s-sakurai <sakuraishunsuke@shunsuke-iMac.local>
2017-04-07 13:30:04 +0900

    initial commit

---
+++ PassCodeL.framework/Headers/PassCodeL.h
@@ -0,0 +1,19 @@
+//
+//  PassCodeL.h
+//  PassCodeL
+//
+//  Created by s-sakurai on 2017/04/07.
+//  Copyright © 2017年 s-sakurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PassCodeL.
+FOUNDATION_EXPORT double PassCodeLVersionNumber;
+
+//! Project version string for PassCodeL.
+FOUNDATION_EXPORT const unsigned char PassCodeLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PassCodeL/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,18 @@
+PassCodeLSwiftDemo
+=============================
+A Touch ID and Passcode authentication framework(PassCodeL) used in the iOS-Swift Demo app.
+PassCodeL is framework that created by s-sakurai in swift  
+
+<img src="https://github.com/shunabcd/PassCodeLSwiftDemo/blob/master/demo.gif" width="424" height="755">
+
+
+## Requirement
+- (Xcode 8.3 ,Swift 3.1)　or later
+
+## Installation
+- Clone or download
+
+## Licence
+
+MIT license. See LICENSE for details.
+
